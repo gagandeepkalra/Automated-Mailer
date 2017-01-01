d7d0fa3e9f62217bb4315e4052c696539eafda3d
Gagandeep Kalra <gkalra@expedia.com>
2017-01-01 16:35:45 +0530

    first_commit-initial_requirements_met

---
+++ Readme.txt
@@ -0,0 +1,21 @@
+Instructions:
+
+
+Install Python
+
+Put all these files within the same folder(any folder).
+Double click on application to run it.
+If the previous step fails, go to command line and type "python mailer.py".
+To modify the file right click and open with any text editor.
+
+
+
+mailer.py -
+modify this file to add your loginId and password for gmail account(if not using gmail modify your email server as well).
+you will receive a security mail from google for mail access, open that mail and turn off that security feature, this will allow the python application to send mails.
+
+content.csv -
+if not opening this file using excel, enclose messages you add in double quotes and each message in new line.
+
+receipients.csv -
+add each message in new line.
---
+++ mailer.py
@@ -0,0 +1,58 @@
+import csv
+import smtplib
+ 
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+			  subject, message,
+			  login, password,
+			  smtpserver='smtp.gmail.com:587'):
+	header  = 'From: %s\n' % from_addr
+	header += 'To: %s\n' % ','.join(to_addr_list)
+	header += 'Cc: %s\n' % ','.join(cc_addr_list)
+	header += 'Subject: %s\n' % subject
+	message = header + message
+ 
+	server = smtplib.SMTP(smtpserver)
+	server.starttls()
+	server.login(login,password)
+	problems = server.sendmail(from_addr, to_addr_list, message)
+	server.quit()
+
+
+# Program execution starts from here
+
+message_list = []
+
+# Read all message data from the csv file.
+with open('content.csv', 'rb') as b:
+	messages = csv.reader(b)
+	message_list.extend(messages)
+
+receipients = []
+
+# Read list of receipients
+with open('receipients.csv', 'rb') as b:
+	receivers = b.readlines()
+	for receiver in receivers:
+		receipients.extend(receiver.split())
+
+
+# Sending E-Mail
+for message_row in message_list:
+	if len(message_row) == 1:
+		# trying to send a mail
+		try:
+			sendemail(from_addr    = '', 
+                      to_addr_list = receipients,
+                      cc_addr_list = [], 
+                      subject      = 'Howdy', 
+                      message      = message_row[0], 
+                      login        = '', 
+                      password     = '')
+
+			message_row.append('mail sent')
+		except Exception as e:
+			raise e
+
+with open('content.csv', 'wb') as b:
+	writer = csv.writer(b)
+	writer.writerows(message_list)
